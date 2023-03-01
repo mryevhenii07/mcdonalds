c244b8d4bd4b5a994c05838dd70bd3811b94d14d
mryevhenii07 <mrit.works07@gmail.com>
2023-03-01 15:40:18 +0200

    added header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,27 @@
+import {FC} from 'react'
+import s from './Header.module.css';
+import logo from '../../image/logo-header.png';
+import {IoLocationSharp,IoSearchOutline} from 'react-icons/io5';
+
+
+ const Header:FC = () => {
+  return (
+    <div className={s.header}>
+        <div className={`${s.headerWrap} ${s.gridInit } ${s.grid } `}>
+
+            <div className={`${s.wrapLogo} ${s.boxInit } ${s.box } ${s.lLogo }`}>  <img src={logo} alt="Logo" width={70} height={60} /></div>
+            <div className={` ${s.boxInit } ${s.box } ${s.lSection }`}>
+                <div>Відкриті ресторани</div>
+                <div className={s.wrapSearchLocation}>
+                    <button className={s.search}><IoSearchOutline className={s.searchIcon}/> Пошук</button>
+                    <button className={s.location}><IoLocationSharp className={s.locationIcon}/>Знайти нас</button>
+                </div>
+            </div>
+            <nav className={` ${s.boxInit } ${s.box } ${s.lNav }`}>Lorem ips neque, quasi rerum modi autem excepturi?</nav>
+       
+        </div>
+    </div>
+  )
+}
+
+export default Header
